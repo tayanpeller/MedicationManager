3bb1f49976b2a7f59a52b95c7d85dd5163478856
tayan <tayanpeller22@gmail.com>
2026-03-08 19:18:40 +0000

    feat: add hospital table

---
+++ models/hospitals.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Integer, Boolean, String, DateTime, Column
+from sqlalchemy.sql import func
+from sqlalchemy.orm import declarative_base, relationship
+import enum
+
+Base = declarative_base()
+
+class Hospitals(Base):
+    __tablename__ = "hospitals"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_active = Column(Boolean, default=True)
+
+    doctors= relationship("Doctors", back_populates="hospital")
