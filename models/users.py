28439dcb2336fb3c993a9b8cb83d9a186540f4fd
tayan <tayanpeller22@gmail.com>
2026-03-08 19:20:39 +0000

    fix: unique email for users

--- models/users.py
+++ models/users.py
@@ -10,7 +10,7 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     firstname = Column(String, nullable=False)
     lastname = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     hash_pwd = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
