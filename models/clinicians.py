d7424326eeed73ae42cf9b08312bed67df4bef25
tayan <tayanpeller22@gmail.com>
2026-03-09 18:51:51 +0000

    feat: added clinicians table

---
+++ models/clinicians.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from users import Users
+
+
+class Clinicians(Users):
+    __tablename__ = "clinicians"
+
+    doctor_id = Column(Integer, ForeignKey("doctors.id"), nullable=False)
+    doctor = relationship("Doctors", back_populates="clinicians")
+
+    hospital_id = Column(Integer, ForeignKey("hospitals.id"), nullable=False)
+    hospital = relationship("Hospitals", back_populates="clinicians")
+
+    prescription = relationship("Prescriptions", back_populates="clinician")
