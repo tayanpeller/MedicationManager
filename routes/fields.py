b373e780c734bbcbb37e5ceb0ba06837c1704091
tayan <tayanpeller22@gmail.com>
2026-03-10 18:47:38 +0000

    feat: create field endpoint

--- routes/fields.py
+++ routes/fields.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models.fields import Fields
-from schemas.fields import FieldsPrivate, FieldsResponse
+from schemas.fields import FieldsResponse
 from sqlalchemy.orm import Session
 from db.config import get_db
 from typing import List
@@ -18,3 +18,20 @@ def get_all_fields(db: Session = Depends(get_db)):
         )
     
     return fields_list
+
+
+@router.post("", response_model=FieldsResponse)
+def create_field(field: FieldsResponse, db: Session = Depends(get_db)):
+    new_field = Fields(description=field.description)
+
+    if not new_field:
+        raise HTTPException(
+            status_code=404,
+            detail="Not a Valid Field"
+        )
+    
+    db.add(new_field)
+    db.commit()
+    db.refresh(new_field)
+
+    return new_field
