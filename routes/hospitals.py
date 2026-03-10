3222945d2865ab57ceef8431b438e9f636fe1e82
tayan <tayanpeller22@gmail.com>
2026-03-10 16:05:04 +0000

    feat: hospitals get endpoints

--- routes/hospitals.py
+++ routes/hospitals.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List
 from sqlalchemy.orm import Session
 from models.hospitals import Hospitals
 from schemas.hospitals import *
@@ -15,3 +16,21 @@ def hospital_create(hospital: HospitalCreate, db: Session = Depends(get_db)):
     db.refresh(new_hospital)
 
     return new_hospital
+
+@router.get("", response_model=List[HospitalRead])
+def hospital_get(db: Session = Depends(get_db)):
+    hospitals = db.query(Hospitals).all()
+
+    return hospitals
+ 
+@router.get("/{hospital_id}", response_model=HospitalRead)
+def hospital_get_by_id(hospital_id: int, db: Session = Depends(get_db)):
+    hospital = db.query(Hospitals).filter(Hospitals.id == hospital_id).first()
+
+    if not hospital:
+        raise HTTPException(
+            status_code=404,
+            detail="Hospital Not Found"
+        )
+    
+    return hospital
