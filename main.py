a484d4aa8d7f0f813ceecf89dcd5792be3b8ac8e
tayan <tayanpeller22@gmail.com>
2026-03-10 18:48:09 +0000

    feat: fields post route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status
 from routes.healthy_check import router as health_checker
 from routes.hospitals import router as hospitals_router
+from routes.fields import router as fields_router
 
 
 
@@ -14,3 +15,5 @@ async def root() -> str:
 app.include_router(health_checker)
 
 app.include_router(hospitals_router)
+
+app.include_router(fields_router)
