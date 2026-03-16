9cdff4495d0a1e807e94945b9936e13b0af3bd20
tayan <tayanpeller22@gmail.com>
2026-03-16 15:16:00 +0000

    feat: new libs and auth

---
+++ auth/password.py
@@ -0,0 +1,10 @@
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
+
+
+def get_password_hash(password: str) -> str:
+    return password_hash.hash(password=password)
+
+def verify_password(plain_password: str, hash_password: str) -> bool:
+    return password_hash.verify(password=plain_password, hash=hash_password)
---
+++ auth/token.py
@@ -0,0 +1,14 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from core.config import settings
+from datetime import datetime, timedelta, timezone
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
